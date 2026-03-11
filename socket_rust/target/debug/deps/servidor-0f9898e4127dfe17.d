4676697657fc6eebbe2309040d454a143fcb281f
Yes-ID33 <148925837+Yes-ID33@users.noreply.github.com>
2026-03-11 17:16:44 -0500

    reestructuración de carpetas, settings.json, .csproj y .sln

---
+++ socket_rust/target/debug/deps/servidor-0f9898e4127dfe17.d
@@ -0,0 +1,5 @@
+C:\Uni\2026 - 1\Redes de datos 2\SocketBuilds\socket_rust\target\debug\deps\servidor-0f9898e4127dfe17.d: src\bin\servidor.rs
+
+C:\Uni\2026 - 1\Redes de datos 2\SocketBuilds\socket_rust\target\debug\deps\libservidor-0f9898e4127dfe17.rmeta: src\bin\servidor.rs
+
+src\bin\servidor.rs:
